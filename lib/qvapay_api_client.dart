11ca4a08752a433162289fc34d6160fd7de16060
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-09-30 11:54:21 -0500

    rearrange: updated library file to export classes
    added the `models.dart` to export all mododels.

--- lib/qvapay_api_client.dart
+++ lib/qvapay_api_client.dart
@@ -1,3 +1,6 @@
 library qvapay_api_client;
 
+export 'src/exception.dart';
+export 'src/models/models.dart';
+export 'src/qvapay_api.dart';
 export 'src/qvapay_api_client.dart';
---
+++ lib/src/models/models.dart
@@ -0,0 +1,8 @@
+export 'package:qvapay_api_client/src/models/app.dart';
+export 'package:qvapay_api_client/src/models/latest_transaction.dart';
+export 'package:qvapay_api_client/src/models/me.dart';
+export 'package:qvapay_api_client/src/models/owner.dart';
+export 'package:qvapay_api_client/src/models/paid_by.dart';
+export 'package:qvapay_api_client/src/models/service.dart';
+export 'package:qvapay_api_client/src/models/service_buy.dart';
+export 'package:qvapay_api_client/src/models/wallet.dart';
